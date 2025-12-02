d627a813bbb513e14a49fb7d86f17c548eb61128
Procech20 <prophete.isingizwe@codeland.it>
2025-12-01 19:56:26 -0600

    added some ui improvements and allowed cross-platform backend compilation

--- server.cpp
+++ server.cpp
@@ -1,12 +1,56 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
+#ifdef _WIN32
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+typedef SOCKET socket_t;
+#define CLOSESOCK closesocket
+#define SOCK_INIT()                                \
+    do                                             \
+    {                                              \
+        WSADATA wsa;                               \
+        if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) \
+        {                                          \
+            std::cerr << "WSAStartup failed\n";    \
+            std::exit(1);                          \
+        }                                          \
+    } while (0)
+#define SOCK_CLEANUP() WSACleanup()
+
+#else
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <ctime>
+#include <sys/stat.h>
+
+typedef int socket_t;
+#define CLOSESOCK close
+#define SOCK_INIT() \
+    do              \
+    {               \
+    } while (0)
+#define SOCK_CLEANUP() \
+    do                 \
+    {                  \
+    } while (0)
+#endif
+
+// COMMON HEADERS GO BELOW THIS LINE
+#include <iostream>
+#include <fstream>
 #include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+#include <map>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
 #include "Order.h"
 #include "OrderHistory.h"
 #include "AccountRegistration.h"
@@ -49,18 +93,47 @@ string url_decode(const string &str)
 
 void send_file(int client_fd, const string &filename)
 {
-    ifstream file(filename);
+    // Open in binary mode so images (jpg/png) are sent correctly
+    ifstream file(filename, ios::binary);
     if (!file)
     {
         string resp = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
         send(client_fd, resp.c_str(), resp.size(), 0);
         return;
     }
+
+    // Read whole file into memory
     string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    string header = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n";
+
+    // Determine Content-Type from file extension
+    string contentType = "text/html; charset=utf-8";
+    auto dotPos = filename.find_last_of('.');
+    if (dotPos != string::npos)
+    {
+        string ext = filename.substr(dotPos + 1);
+        for (auto &c : ext)
+            c = static_cast<char>(tolower(c));
+
+        if (ext == "jpg" || ext == "jpeg")
+            contentType = "image/jpeg";
+        else if (ext == "png")
+            contentType = "image/png";
+        else if (ext == "gif")
+            contentType = "image/gif";
+        else if (ext == "css")
+            contentType = "text/css; charset=utf-8";
+        else if (ext == "js")
+            contentType = "application/javascript";
+        else if (ext == "ico")
+            contentType = "image/x-icon";
+        // you can add more (svg, webp, etc.) if needed
+    }
+
+    string header = "HTTP/1.1 200 OK\r\nContent-Type: " + contentType + "\r\n";
     header += "Content-Length: " + to_string(content.size()) + "\r\n\r\n";
+
     send(client_fd, header.c_str(), header.size(), 0);
-    send(client_fd, content.c_str(), content.size(), 0);
+    send(client_fd, content.data(), content.size(), 0);
 }
 
 void handle_json(const string &path, const string &json_str)
@@ -86,7 +159,7 @@ void handle_client(int client_fd)
     int bytes = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
     if (bytes <= 0)
     {
-        close(client_fd);
+        CLOSESOCK(client_fd);
         return;
     }
     buffer[bytes] = 0;
@@ -121,7 +194,7 @@ void handle_client(int client_fd)
                 cout << "File not found: " << history_file << endl;
                 string resp = "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: 0\r\n\r\n";
                 send(client_fd, resp.c_str(), resp.size(), 0);
-                close(client_fd);
+                CLOSESOCK(client_fd);
                 return;
             }
 
@@ -131,7 +204,7 @@ void handle_client(int client_fd)
             header += "Content-Length: " + to_string(content.size()) + "\r\n\r\n";
             send(client_fd, header.c_str(), header.size(), 0);
             send(client_fd, content.c_str(), content.size(), 0);
-            close(client_fd);
+            CLOSESOCK(client_fd);
             return;
         }
 
@@ -141,7 +214,7 @@ void handle_client(int client_fd)
             file = path.substr(1); // Remove leading /
         }
         send_file(client_fd, file);
-        close(client_fd);
+        CLOSESOCK(client_fd);
         return;
     }
 
@@ -156,24 +229,25 @@ void handle_client(int client_fd)
             header += "Content-Length: " + to_string(body_resp.size()) + "\r\n\r\n";
             send(client_fd, header.c_str(), header.size(), 0);
             send(client_fd, body_resp.c_str(), body_resp.size(), 0);
-            close(client_fd);
+            CLOSESOCK(client_fd);
             return;
         }
         handle_json(path, body);
         string resp = "HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK";
         send(client_fd, resp.c_str(), resp.size(), 0);
-        close(client_fd);
+        CLOSESOCK(client_fd);
         return;
     }
 
     // Default: 404
     string resp = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n";
     send(client_fd, resp.c_str(), resp.size(), 0);
-    close(client_fd);
+    CLOSESOCK(client_fd);
 }
 
 int main()
 {
+    SOCK_INIT();
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0)
     {
@@ -182,7 +256,21 @@ int main()
     }
 
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#ifdef _WIN32
+    // Winsock: 4th arg is const char*
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   (const char *)&opt, sizeof(opt)) == SOCKET_ERROR)
+    {
+        std::cerr << "setsockopt() failed\n";
+    }
+#else
+    // POSIX: 4th arg is const void*
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                   &opt, sizeof(opt)) < 0)
+    {
+        std::cerr << "setsockopt() failed\n";
+    }
+#endif
 
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
@@ -203,7 +291,7 @@ int main()
         if (client_fd >= 0)
             handle_client(client_fd);
     }
-    close(server_fd);
+    CLOSESOCK(server_fd);
     return 0;
 }
 
